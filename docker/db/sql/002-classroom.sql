d2961169cfff302be168443c6e807f38ad7420e6
Similar Su <similarsu@qq.com>
2018-08-20 09:43:11 +0800

    add support for db

---
+++ docker/db/sql/002-classroom.sql
@@ -0,0 +1,6 @@
+START TRANSACTION;
+INSERT INTO `classroom` (`id`, `name`) VALUES (1, '初级班');
+INSERT INTO `classroom` (`id`, `name`) VALUES (2, '中级班');
+INSERT INTO `classroom` (`id`, `name`) VALUES (3, '高级班');
+
+COMMIT;
---
+++ docker/db/sql/003-student.sql
@@ -0,0 +1,13 @@
+START TRANSACTION;
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (1, '张团体', 12, 1);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (2, '冷森森', 13, 1);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (3, '吴彦祖', 12, 1);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (4, '李冰冰', 14, 1);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (5, '乌齐雅', 15, 1);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (6, '劳伦斯', 15, 2);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (7, '黑色人', 16, 2);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (8, '榴霰弹', 16, 2);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (9, '镜花缘', 20, 3);
+INSERT INTO `student` (`id`, `name`, `age`, `c_id`) VALUES (10, '威尔逊', 21, 3);
+
+COMMIT;
